2cdff69e65edae2b746cb4bf9df864d8121b3849
Brett Karr <b.karr@polyclip.us>
2019-11-16 14:52:08 -0600

    commit with styling

--- reducer-project/src/App.js
+++ reducer-project/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Todo List - Reducer Project:</h1>
         <TodoList />
       </header>
     </div>
--- reducer-project/src/reducers/reducers.jsx
+++ reducer-project/src/reducers/reducers.jsx
@@ -41,11 +41,3 @@ export const Reducer = (state, action) => {
   }
   return { state };
 };
-
-export const reducer = (state, action) => {
-  if (action.type === "increment") {
-    return { count: state.count + 1 };
-  } else if (action.type === "decrement") {
-    return { count: state.count - 1 };
-  }
-};
